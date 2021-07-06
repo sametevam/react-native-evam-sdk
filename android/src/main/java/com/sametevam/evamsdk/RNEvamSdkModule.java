1320388dd28d03c84ea412120366be43221eab05
sametevam <69842061+sametevam@users.noreply.github.com>
2021-07-06 15:57:47 +0300

    Update RNEvamSdkModule.java

--- android/src/main/java/com/sametevam/evamsdk/RNEvamSdkModule.java
+++ android/src/main/java/com/sametevam/evamsdk/RNEvamSdkModule.java
@@ -1,13 +1,15 @@
 package com.sametevam.evamsdk;
 
+import com.evam.evamsdk.Evam;
+import com.evam.evamsdk.EvamLogManager;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 
+import android.app.Application;
 import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.pm.PackageManager;
+
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,7 +19,7 @@ public class RNEvamSdkModule extends ReactContextBaseJavaModule {
   private final ReactApplicationContext reactContext;
 
   private static final String START = "start";
- 
+  private static final String EVENT = "getEvent";
 
   public RNEvamSdkModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -25,12 +27,12 @@ public class RNEvamSdkModule extends ReactContextBaseJavaModule {
   }
 
   @ReactMethod
-    public String getEvamFunc() { 
-    
-      com.evam.evamsdk.Evam evamInstance = com.evam.evamsdk.Evam.Companion.getInstance();
-        Application thisApplication = getApplication();
+    public String startEvam() {
+      Application application = (Application) reactContext.getApplicationContext();
+      Evam evamInstance = Evam.Companion.getInstance();
+
         evamInstance.initialize(
-                thisApplication,
+                application,
               "https://test.em.api-evam.com/sdk-api/in-app-communication-wrapper",
                 "https://test.em.api-evam.com/sdk-listener/event",
                 "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJldmFtIiwiZG9tYWluIjoiZXZhbS5jb20iLCJpYXQiOjE2MTQyODYyODZ9.sfhJmjaYjeVMFSdR7nJxg1SndWHqdJY0HgdICmnpBgg",
@@ -39,8 +41,30 @@ public class RNEvamSdkModule extends ReactContextBaseJavaModule {
                 "evam.com",
                 false
         );
+
+    System.out.println("getEvamFunc");
+
+    evamInstance.updateActorId(reactContext, "samet");
     
-      return "heyyyyy"; 
+    return Boolean.toString(evamInstance.isInitialized());
+  }
+
+  @ReactMethod
+  public String sendEvamEvent() {
+
+    System.out.println("çalıştı mı");
+    HashMap<String, String> eventParameters = new HashMap<>();
+    eventParameters.put("key1", "cem");
+    eventParameters.put("key2", "samet");
+    try{
+      EvamLogManager.Companion.getInstance(reactContext)
+              .logEvent(reactContext, "event_name", eventParameters);
+    }
+    catch(Exception e) {
+      e.printStackTrace();
+    }
+
+    return "success";
   }
 
   @Override
@@ -69,7 +93,9 @@ public class RNEvamSdkModule extends ReactContextBaseJavaModule {
         // evamInstance.setSplashActivity(MainActivity.class);
 
         // constants.put(START, "May the force be with you");
-        constants.put(START, getEvamFunc());
+
+        constants.put(START, startEvam());
+        constants.put(EVENT, sendEvamEvent());
     // } catch (NameNotFoundException e) {
       // e.printStackTrace();
       //  System.out.println("Message: " + e.getMessage());
