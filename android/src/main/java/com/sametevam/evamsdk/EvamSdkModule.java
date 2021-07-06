4758a6302e9c6d3261c7110b527c6d6766a838f1
sametevam <69842061+sametevam@users.noreply.github.com>
2021-07-06 11:08:18 +0300

    Update EvamSdkModule.java

--- android/src/main/java/com/sametevam/evamsdk/EvamSdkModule.java
+++ android/src/main/java/com/sametevam/evamsdk/EvamSdkModule.java
@@ -12,21 +12,48 @@ import android.content.pm.PackageManager;
 import java.util.HashMap;
 import java.util.Map;
 
-public class EvamSdkModule extends ReactContextBaseJavaModule {
+public class RNEvamSdkModule extends ReactContextBaseJavaModule {
 
   private final ReactApplicationContext reactContext;
 
   private static final String START = "start";
  
 
-  public EvamSdkModule(ReactApplicationContext reactContext) {
+  public RNEvamSdkModule(ReactApplicationContext reactContext) {
     super(reactContext);
     this.reactContext = reactContext;
   }
 
   @Override
-  public String start() {
-    return "start başla";
+  public String getName() {
+    return "RNEvamSdk";
+  }
+
+   @Override
+  public Map<String, Object> getConstants() {
+    final Map<String, Object> constants = new HashMap<>();
+
+    // try {
+       com.evam.evamsdk.Evam evamInstance = com.evam.evamsdk.Evam.Companion.getInstance();
+        Application thisApplication = getApplication();
+        evamInstance.initialize(
+                thisApplication,
+              "https://test.em.api-evam.com/sdk-api/in-app-communication-wrapper",
+                "https://test.em.api-evam.com/sdk-listener/event",
+                "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJldmFtIiwiZG9tYWluIjoiZXZhbS5jb20iLCJpYXQiOjE2MTQyODYyODZ9.sfhJmjaYjeVMFSdR7nJxg1SndWHqdJY0HgdICmnpBgg",
+                "evam.com",
+                "evam",
+                "evam.com",
+                false
+        );
+        // evamInstance.setSplashActivity(MainActivity.class);
+
+        constants.put(START, "May the force be with you");
+    // } catch (NameNotFoundException e) {
+      // e.printStackTrace();
+      //  System.out.println("Message: " + e.getMessage());
+    // }
+    return constants;
   }
 
 }
