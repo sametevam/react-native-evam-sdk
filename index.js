d56e3f9d82fd0f94ce31bf2eb716757e60f15446
sametevam <69842061+sametevam@users.noreply.github.com>
2021-08-12 12:46:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,39 @@ import { NativeModules } from 'react-native';
 
 const { RNEvamSdk } = NativeModules;
 
-type EvamSdkObject = {
-  start: ?string,
-  event: ?string,
-};
+const EvamSdk = {};
 
-const EvamSdk: EvamSdkObject = {
-  start: RNEvamSdk && RNEvamSdk.start,
-  event: RNEvamSdk && RNEvamSdk.event,
+// type EvamSdkObject = {
+//   start: ?string,
+//   event: ?string,
+// };
+
+// const EvamSdk: EvamSdkObject = {
+//   start: RNEvamSdk && RNEvamSdk.start,
+//   event: RNEvamSdk && RNEvamSdk.event,
   
-};
+// };
+
+function startEvam(string): Promise<string> {
+  return RNEvamSdk.startEvam(string);
+}
+
+function sendEvent(eventName): Promise<string> {
+    return RNEvamSdk.sendEvamEvent(eventName);
+}
+
+function changeActorId(actorId): Promise<string> {
+  return RNEvamSdk.changeActorId(actorId);
+}
+
+function sendNavigationLog(page): Promise<string> {
+  return RNEvamSdk.sendNavigationLog(page);
+}
+
+
+EvamSdk.startEvam = startEvam;
+EvamSdk.sendEvent = sendEvent;
+EvamSdk.changeActorId = changeActorId;
+EvamSdk.sendNavigationLog = sendNavigationLog;
 
 export default EvamSdk;
